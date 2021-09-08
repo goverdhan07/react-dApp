249ce825f29753bb2b8b3ee87022a1c1a1f8f172
goverdhan07 <66107302+goverdhan07@users.noreply.github.com>
2021-09-08 21:43:13 +0530

    update - async function - fetchGreeting()

--- src/App.js
+++ src/App.js
@@ -9,7 +9,33 @@ const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 function App() {
   const [greeting, setGreetingValue] = useState()
   
+  //These are asynchronous functions cuz these guys neet to await for returning some value.
+  
+  async function requestAccount() { //requesAccount is going to get connected with metamask wallet of the user when we need to create a transaction.
+    
+  }
 
+  async function fetchGreeting() { 
+    if (typeof window.ethereum !== 'undefined') { //checks whether the metamask is present in the system or not
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greetAddress, Greeter.abi, provider) 
+      try {
+        const data = await contract.great() //value we are reading from the blockchain
+        console.log('data',data)
+      }catch(err) {
+        console.log("Error: ", err)
+        
+      }
+
+
+
+    }
+
+  }
+
+  async function setGreeting() {
+
+  }
 
 
 
